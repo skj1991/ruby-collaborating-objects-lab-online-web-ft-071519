33501f08709e0c937e7107479c2c1bc1605466f9
Stacy Johns <stacykjohns@icloud.com>
2019-09-04 19:54:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,20 +6,20 @@ class Artist
     @name = name
   end
 
-  def add_song(song)
-    song.artist = self
-    @@song_count += 1
-  end
+  #def add_song(song)
+  #  song.artist = self
+  #  @@song_count += 1
+  #end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
+  #def songs
+  #  Song.all.select {|song| song.artist == self}
+  #end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
-    @@song_count += 1
-  end
+  #def add_song_by_name(name)
+  #  song = Song.new(name)
+  #  add_song(song)
+  #  @@song_count += 1
+  #end
 
 
 end
