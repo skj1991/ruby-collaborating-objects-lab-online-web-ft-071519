ad00d4140ab220cb696bfbfa8effde48ec52bb39
Stacy Johns <stacykjohns@icloud.com>
2019-09-04 19:56:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,19 @@
 class Artist
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    save
   end
 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
   #def add_song(song)
   #  song.artist = self
   #  @@song_count += 1
