3f0a24b0437e490e5b84b0529cc5c720f36a0363
Stacy Johns <stacykjohns@icloud.com>
2019-09-04 20:24:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,14 @@ class Song
       @@all
     end
 
-    def artist_name=(name)
-      if self.artist.nil?
-        self.artist = Artist.new(name)
-      else
-        self.artist.name = name
-      end
+    def self.new_by_filename(filename)
+
     end
+    #def artist_name=(name)
+    #  if self.artist.nil?
+    #    self.artist = Artist.new(name)
+    #  else
+    #    self.artist.name = name
+    #  end
+    #end
 end
