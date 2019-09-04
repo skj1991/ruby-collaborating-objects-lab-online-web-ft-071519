65e76d78f0d6757270a34121a1506666eec6e532
Stacy Johns <stacykjohns@icloud.com>
2019-09-04 21:00:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,6 +21,16 @@ class Artist
     song.artist = self
   end
 
-  
+  def self.find_or_create_by_name
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 
 end
