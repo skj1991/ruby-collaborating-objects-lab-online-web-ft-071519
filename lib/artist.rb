4b696887a64170604f1dac315432184f686fab64
Stacy Johns <stacykjohns@icloud.com>
2019-09-04 21:05:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,11 +22,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.find(name)?
-      self.find(name)
-    else
-      self.new(name)
-    end
+    self.find(name) ? self.find(name) : self.new(name)
   end
 
   def self.find(name)
